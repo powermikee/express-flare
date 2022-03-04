18d945ba7eb6e68ce51fce17eef8d737c1ee7556
Michael Trudgeon <michael@usercentric.com.au>
2022-03-04 23:37:28 +1100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ignorePatterns: ['**/*.ts'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'arrow-body-style': [0],
+  },
+};
